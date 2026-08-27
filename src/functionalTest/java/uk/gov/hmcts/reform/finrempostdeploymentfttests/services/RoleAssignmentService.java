b5a3d70189abedcc6877dfa91bb3dd4865cdb2ee
Sahil Goonraz <sahil.goonraz@solirius.com>
2026-08-27 15:17:08 +0100

    DFR-5508: Address comment

--- src/functionalTest/java/uk/gov/hmcts/reform/finrempostdeploymentfttests/services/RoleAssignmentService.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/finrempostdeploymentfttests/services/RoleAssignmentService.java
@@ -116,10 +116,7 @@ public class RoleAssignmentService {
     public void setupRoleAssignment(Headers headers, UserInfo userInfo, String roleName) {
 
         String authorizations = JsonUtil.toJsonString(List.of());
-        if (roleName.equals("ctsc")) {
-            authorizations = JsonUtil.toJsonString(List.of("SKILL:ABA2:ManageScannedDocuments",
-                                                           "SKILL:ABA2:CheckingApplications"));
-        } else if (roleName.equals("ctsc-team-leader")) {
+        if (roleName.equals("ctsc") || roleName.equals("ctsc-team-leader")) {
             authorizations = JsonUtil.toJsonString(List.of("SKILL:ABA2:ManageScannedDocuments",
                                                            "SKILL:ABA2:CheckingApplications"));
         }
